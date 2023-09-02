31d639b4cbb51411211a823eb6a81ec2101eb240
SirMorfield <joppe@joppekoers.nl>
2023-09-02 16:35:02 +0200

    Implement as_formatted_str() trait

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,5 @@
+use pad::{Alignment, PadStr};
 use std::os::raw::c_char;
-// use std::os::raw::c_int;
 
 pub fn vector_to_cstring_vector(vector: Vec<String>) -> *mut *mut c_char {
     let mut cstring_vector: Vec<std::ffi::CString> = Vec::new();
@@ -13,4 +13,56 @@ pub fn vector_to_cstring_vector(vector: Vec<String>) -> *mut *mut c_char {
     let cstring_vector_ptr_ptr: *mut *mut c_char = cstring_vector_ptr.as_mut_ptr();
     std::mem::forget(cstring_vector_ptr);
     cstring_vector_ptr_ptr
+}
+
+pub trait AsFormattedString {
+    fn as_formatted_str(&self) -> String;
+}
+
+impl AsFormattedString for std::time::Duration {
+    fn as_formatted_str(&self) -> String {
+        let mut result = String::new();
+        result.reserve(22); // "00h 00m 04.668 668 668".len() == 22
+
+        let seconds = self.as_secs();
+        let hours = seconds / 3600;
+        let minutes = (seconds % 3600) / 60;
+        let seconds = std::time::Duration::from_secs(seconds % 60);
+
+        result.push_str(&hours.to_string().pad(2, '0', Alignment::Right, false));
+        result.push_str("h ");
+        result.push_str(&minutes.to_string().pad(2, '0', Alignment::Right, true));
+        result.push_str("m ");
+        result.push_str(
+            &seconds
+                .as_secs()
+                .to_string()
+                .pad(2, '0', Alignment::Right, true),
+        );
+        result.push_str(".");
+
+        result.push_str(
+            &self
+                .subsec_millis()
+                .to_string()
+                .pad(3, '0', Alignment::Right, true),
+        );
+        result.push_str(" ");
+        result.push_str(
+            &self
+                .subsec_micros()
+                .to_string()
+                .pad(3, '0', Alignment::Right, true),
+        );
+        result.push_str(" ");
+        result.push_str(
+            &self
+                .subsec_nanos()
+                .to_string()
+                .pad(3, '0', Alignment::Right, true),
+        );
+        result.push_str("s");
+
+        return result;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 mod helpers;
+use helpers::AsFormattedString;
 use helpers::*;
 
 mod foreign_function_interfaces;
 use foreign_function_interfaces::*;
+use std::time;
 
-fn main() {
+fn cpp_main() -> i32 {
     let result: i32;
     unsafe {
         let argv = std::env::args().collect::<Vec<_>>();
-        for arg in &argv {
-            println!("The arg is: {}", arg);
-        }
         result = main2(argv.len() as i32, vector_to_cstring_vector(argv));
     }
-    println!("The result from C is: {}", result);
+    return result;
+}
+
+fn main() {
+    let start = time::Instant::now();
+    let exit_code = cpp_main();
+    let duration = start.elapsed();
+    println!("Exit code: {}", exit_code);
+    println!("Time elapsed: {}", duration.as_formatted_str());
 }
