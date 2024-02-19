ad47233ae01ec12679509aec5ee32a17065a438b
Joppe Koers <joppe@joppekoers.nl>
2024-02-19 13:47:11 +0100

    better dir

--- src/camera.rs
+++ src/camera.rs
@@ -32,23 +32,40 @@ impl Camera {
         }
     }
 
+    pub fn update_dir_pitch_yaw(&mut self, pitch_delta: f32, yaw_delta: f32) {
+        let pitch = f32::asin(self.dir.y) + pitch_delta;
+        let yaw = f32::atan2(self.dir.x, self.dir.z) + yaw_delta;
+
+        self.dir = Point::new(
+            yaw.sin() * pitch.cos(),
+            pitch.sin(),
+            yaw.cos() * pitch.cos(),
+        )
+        .to_normalized();
+    }
+
+    pub fn update_right_left(&mut self, delta: f32) {
+        let left = self.dir.cross(&Point::new(0.0, 1.0, 0.0)).to_normalized();
+        self.pos += left * delta;
+    }
+
     pub fn move_to(&mut self, dir: Direction, amount: f32) {
         match dir {
             Direction::Up => self.pos.y += amount,
             Direction::Down => self.pos.y -= amount,
-            Direction::Left => self.pos.x -= amount,
-            Direction::Right => self.pos.x += amount,
+            Direction::Left => self.update_right_left(amount),
+            Direction::Right => self.update_right_left(-amount),
             Direction::Forward => self.pos += self.dir * amount,
             Direction::Backward => self.pos -= self.dir * amount,
-            Direction::PitchUp => self.dir.y += amount,
-            Direction::PitchDown => self.dir.y -= amount,
-            Direction::YawLeft => self.dir.x -= amount,
-            Direction::YawRight => self.dir.x += amount,
+            Direction::PitchUp => self.update_dir_pitch_yaw(amount, 0.0),
+            Direction::PitchDown => self.update_dir_pitch_yaw(-amount, 0.0),
+            Direction::YawLeft => self.update_dir_pitch_yaw(0.0, amount),
+            Direction::YawRight => self.update_dir_pitch_yaw(0.0, -amount),
         }
     }
     pub fn keyboard(&mut self, key: Key) {
         let move_speed = 10.0;
-        let yaw_pitch_speed = 0.1;
+        let yaw_pitch_speed = 0.15;
 
         match key {
             Key::W => self.move_to(Direction::Up, move_speed),
@@ -62,7 +79,7 @@ impl Camera {
             Key::Left => self.move_to(Direction::YawLeft, yaw_pitch_speed),
             Key::Right => self.move_to(Direction::YawRight, yaw_pitch_speed),
 
-            Key::P => println!("pos: {:?}", self.pos),
+            Key::P => println!("pos: {:?}\ndir: {:?}", self.pos, self.dir),
             _ => {}
         }
     }
