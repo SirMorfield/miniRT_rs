49f736e6b2da460ea4bfd8b3b442a6460a563b1b
Joppe Koers <joppe@joppekoers.nl>
2024-10-11 17:09:49 +0200

    Add sanity check

--- src/net/socket.rs
+++ src/net/socket.rs
@@ -1,6 +1,9 @@
 use std::io::{ErrorKind, Read, Write};
 use std::net::TcpStream;
 
+const MSG_SIZE_BYTES: usize = 8;
+const CHECKSUM_BYTES: usize = 8;
+
 pub struct NetSocket {
     pub id: u64,
     stream: TcpStream,
@@ -21,9 +24,11 @@ impl NetSocket {
     }
 
     pub fn write(&mut self, message: &[u8]) -> Result<(), std::io::Error> {
-        let message_size_bytes = serialize_u64(message.len() as u64);
-        self.stream.write_all(&message_size_bytes)?;
+        let msg = serialize_usize((message.len() + CHECKSUM_BYTES) as usize);
+        let checksum = self.checksum(message);
+        self.stream.write_all(&msg)?;
         self.stream.write_all(message)?;
+        self.stream.write_all(&serialize_usize(checksum))?;
         Ok(())
     }
 
@@ -32,27 +37,35 @@ impl NetSocket {
 
         loop {
             if self.msg_size.is_none() && self.len >= 8 {
-                self.msg_size = Some(deserialize_u64(&self.buffer[..]) as usize);
-                self.buffer.rotate_left(8);
-                self.len -= 8;
+                self.msg_size = Some(deserialize_usize(&self.buffer[..]) as usize);
+                self.buffer.rotate_left(MSG_SIZE_BYTES);
+                self.len -= MSG_SIZE_BYTES;
+                out.reserve(self.msg_size.unwrap());
             }
-            if let Some(command_size) = self.msg_size {
-                let command_size_leftover = command_size - out.len();
-                let max = self.len.min(command_size_leftover);
+            if let Some(msg_size) = self.msg_size {
+                let msg = msg_size - out.len();
+                let max = self.len.min(msg);
                 out.extend(&self.buffer[..max]);
 
                 // if we used all the bytes in the buffer, there are 0 bytes leftover to use in the next iteration,
                 // so we don't need to rotate
-                if self.len != command_size_leftover {
+                if self.len != msg {
                     self.buffer.rotate_left(max);
                 }
                 self.len -= max;
 
-                if out.len() > command_size {
+                if out.len() > msg_size {
                     panic!("out.len() > command_size");
                 }
-                if out.len() == command_size {
+
+                if out.len() == msg_size {
                     self.msg_size = None;
+                    let checksum = deserialize_usize(&out[msg_size - CHECKSUM_BYTES..]);
+                    out.truncate(out.len() - CHECKSUM_BYTES);
+
+                    if self.checksum(&out) != checksum {
+                        return Err(std::io::Error::new(ErrorKind::Other, "Checksum failed"));
+                    }
                     return Ok(out);
                 }
             }
@@ -63,17 +76,29 @@ impl NetSocket {
             self.len += read;
         }
     }
+
+    fn checksum(&self, message: &[u8]) -> usize {
+        let mut checksum = 0usize;
+        for (i, byte) in message.iter().enumerate() {
+            checksum = checksum.wrapping_add((*byte as usize) + i);
+        }
+        checksum
+    }
+
+    pub fn disconnect(&self) {
+        self.stream.shutdown(std::net::Shutdown::Both).unwrap();
+    }
 }
 
-fn deserialize_u64(vec: &[u8]) -> u64 {
-    let mut out: u64 = 0;
+fn deserialize_usize(vec: &[u8]) -> usize {
+    let mut out: usize = 0;
     for i in 0..8 {
-        out |= (vec[i] as u64) << (i * 8);
+        out |= (vec[i] as usize) << (i * 8);
     }
     out
 }
 
-fn serialize_u64(num: u64) -> Vec<u8> {
+fn serialize_usize(num: usize) -> Vec<u8> {
     let mut out = Vec::with_capacity(8);
     for i in 0..8 {
         out.push((num >> (i * 8)) as u8);
